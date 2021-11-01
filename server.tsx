69a91024083f81d0c2e926921aeb3df86006f5c2
ryoya_miyamoto <ryoya.miyamoto226@gmail.com>
2021-11-01 21:22:49 +0900

    ymlファイル修正

--- server.tsx
+++ server.tsx
@@ -16,45 +16,44 @@ import { WEATHER_OVERVIEW, WEATHER } from "./api.ts";
 const app = createApp();
 
 app.get("/", async (req) => {
-  console.log("通過しました");
-  // //気象庁APIから佐賀県の情報を取得
-  // const { targetArea, headlineText, text }: WEATHER_OVERVIEW_TYPE = await fetch(
-  //   WEATHER_OVERVIEW
-  // )
-  //   .then((res) => res.json())
-  //   .catch((e) => console.log(e));
-  // const weathers = await fetch(WEATHER)
-  //   .then((res) => res.json())
-  //   .catch((e) => console.log(e));
-  // //今日の天気
-  // const todayArea = weathers[0].timeSeries[0].areas[0];
-  // const forecasts: string[] = todayArea.weathers;
+  //気象庁APIから佐賀県の情報を取得
+  const { targetArea, headlineText, text }: WEATHER_OVERVIEW_TYPE = await fetch(
+    WEATHER_OVERVIEW
+  )
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+  const weathers = await fetch(WEATHER)
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+  //今日の天気
+  const todayArea = weathers[0].timeSeries[0].areas[0];
+  const forecasts: string[] = todayArea.weathers;
 
-  // await req.respond({
-  //   status: 200,
-  //   headers: new Headers({
-  //     "content-type": "text/html; charset=UTF-8",
-  //   }),
-  //   body: ReactDOMServer.renderToString(
-  //     <html lang="en">
-  //       <head>
-  //         <title>Document</title>
-  //       </head>
-  //       <body>
-  //         <h1>
-  //           <div>{targetArea}</div>
-  //           <div>{headlineText}</div>
-  //           <div>{text}</div>
-  //         </h1>
-  //         <ul>
-  //           {forecasts.map((m) => (
-  //             <li key={m}>{m}</li>
-  //           ))}
-  //         </ul>
-  //       </body>
-  //     </html>
-  //   ),
-  // });
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      "content-type": "text/html; charset=UTF-8",
+    }),
+    body: ReactDOMServer.renderToString(
+      <html lang="en">
+        <head>
+          <title>Document</title>
+        </head>
+        <body>
+          <h1>
+            <div>{targetArea}</div>
+            <div>{headlineText}</div>
+            <div>{text}</div>
+          </h1>
+          <ul>
+            {forecasts.map((m) => (
+              <li key={m}>{m}</li>
+            ))}
+          </ul>
+        </body>
+      </html>
+    ),
+  });
 });
 
 app.listen({ port: 80 });
