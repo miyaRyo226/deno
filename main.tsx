ac999ee3aa01baea4c5ec6e316f2e4dab38001e5
ryoya_miyamoto <ryoya.miyamoto226@gmail.com>
2021-11-03 22:39:54 +0900

    リクエスト時の設定変更

--- main.tsx
+++ main.tsx
@@ -45,34 +45,31 @@ const App = ({
 );
 
 const handler = async (req: Request): Promise<Response> => {
-  //GETリクエストの時
-  if (req.method == "GET") {
-    //気象庁APIから佐賀県の情報を取得
-    const { targetArea, headlineText, text }: WEATHER_OVERVIEW_TYPE =
-      await fetch(WEATHER_OVERVIEW)
-        .then((res) => res.json())
-        .catch((e) => console.log(e));
-    const weathers = await fetch(WEATHER)
-      .then((res) => res.json())
-      .catch((e) => console.log(e));
-    //今日の天気
-    const todayArea = weathers[0].timeSeries[0].areas[0];
-    const forecasts: string[] = todayArea.weathers;
-    const html = renderSSR(
-      <App
-        targetArea={targetArea}
-        headlineText={headlineText}
-        text={text}
-        forecasts={forecasts}
-      />
-    );
-    return new Response(html, {
-      headers: {
-        "content-type": "text/html",
-      },
-    });
-  }
-  return new Response("Hello world");
+  //気象庁APIから佐賀県の情報を取得
+  const { targetArea, headlineText, text }: WEATHER_OVERVIEW_TYPE = await fetch(
+    WEATHER_OVERVIEW
+  )
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+  const weathers = await fetch(WEATHER)
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+  //今日の天気
+  const todayArea = weathers[0].timeSeries[0].areas[0];
+  const forecasts: string[] = todayArea.weathers;
+  const html = renderSSR(
+    <App
+      targetArea={targetArea}
+      headlineText={headlineText}
+      text={text}
+      forecasts={forecasts}
+    />
+  );
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
 };
 
 console.log("Listening on http://localhost:80");
