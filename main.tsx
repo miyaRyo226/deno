134c0fb2aa4378e32d169d4ecfe44fdc07aae0c3
ryoya_miyamoto <ryoya.miyamoto226@gmail.com>
2021-11-03 22:27:13 +0900

    文字コードの設定を追加

--- main.tsx
+++ main.tsx
@@ -24,6 +24,9 @@ const App = ({
 }: APPTYPE): JSX.ElementClass => (
   <html lang="en">
     <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Document</title>
     </head>
     <body>
